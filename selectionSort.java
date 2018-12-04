b5e57b8a0a8e566428c30ae1740434b4021a0dff
Tyler Huang <thuang00@stuy.edu>
2018-12-04 18:13:43 -0500

    asda

--- selectionSort.java
+++ selectionSort.java
@@ -1,8 +1,10 @@
 public class selectionSort{
   public static void selectionsort(int[] ary){
     for (int i = 1; i < ary.length; i++){
+      //sets the min to the previous term
       int min = ary[i-1];
       for (int j = 1; j < ary.length; j++){
+        // loop through array and replace min with a smaller number
         if (ary[j] < min){
           min = ary[j];
         }
